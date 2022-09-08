48449a70264483ca91cd6f2601b39eea650a5071
JohHod <john.hodkinson@softwire.com>
2022-09-08 14:39:28 +0100

    Removed extra newlines

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,12 @@
 def fizzbuzz
   for number in 1..100
-    print "\n"
     if number%3==0 then print "Fizz" end
     if number%5==0 then print "Buzz" end
     if not number%3==0 and not number%5==0 then print number end
+    if number < 100 then print "\n" end
   end
 end
 
 fizzbuzz
+
+
