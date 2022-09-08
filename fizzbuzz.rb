5fe920539fbc9702522606c032cdd9186f1aae6a
JohHod <john.hodkinson@softwire.com>
2022-09-08 17:39:00 +0100

    variable/function name refactor

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,26 +1,26 @@
-def fizzBuzz(number)
-  outputArray = []
-  if number%3==0 then outputArray.push("Fizz") end
-  if number%13==0 then outputArray.push("Fezz") end
-  if number%5==0 then outputArray.push("Buzz") end
-  if number%7==0 then outputArray.push("Bang") end
+def fizz_buzz(number)
+  output_array = []
+  if number%3==0 then output_array.push("Fizz") end
+  if number%13==0 then output_array.push("Fezz") end
+  if number%5==0 then output_array.push("Buzz") end
+  if number%7==0 then output_array.push("Bang") end
   if number%11==0 then
-    outputArray.select! {|element| element == "Fezz"}
-    outputArray.push ("Bong")
+    output_array.select! {|element| element == "Fezz"}
+    output_array.push ("Bong")
   end
-  if number%17==0 then outputArray.reverse! end
-  if outputArray.length == 0 then outputArray.push(number) end
-  return outputArray.join("")
+  if number%17==0 then output_array.reverse! end
+  if output_array.length == 0 then output_array.push(number) end
+  return output_array.join("")
 end
 
-def fizzBuzzList(max)
+def fizz_buzz_list(max)
   for number in 1..max
-    puts(fizzBuzz(number))
+    puts(fizz_buzz(number))
   end
 end
 
 TestCase = Struct.new(:input, :output)
-fizzBuzzTestCases =
+fizz_buzz_test_cases =
   [
     TestCase.new(15, "FizzBuzz"),
     TestCase.new(21, "FizzBang"),
@@ -32,18 +32,23 @@ fizzBuzzTestCases =
     TestCase.new(255, "BuzzFizz")
   ]
 
-def fizzBuzzTest(testCases)
-  numPass = 0
-  for test in testCases
-    myOutput = fizzBuzz(test.input)
-    result = (myOutput == test.output)
-    puts("Input: "+test.input.to_s+", Expected Output: "+test.output+", Function Output: "+myOutput+" Result: "+result.to_s)
-    if result == true then numPass+=1 end
+def fizz_buzz_test(test_cases)
+  num_pass = 0
+  for test in test_cases
+    my_output = fizz_buzz(test.input)
+    result = (my_output == test.output)
+    puts("Input: "+test.input.to_s+", Expected Output: "+test.output+", Function Output: "+my_output+" Result: "+result.to_s)
+    if result == true then num_pass+=1 end
   end
-  puts("Passing "+numPass.to_s+" / "+testCases.length.to_s+""+(numPass==testCases.length ? " Success!":" ERROR"))
+  puts("Passing "+num_pass.to_s+" / "+test_cases.length.to_s+""+(num_pass==test_cases.length ? " Success!":" ERROR"))
+  puts("FizzBuzz Output:")
 end
 
-fizzBuzzList(100)
-fizzBuzzTest(fizzBuzzTestCases)
+puts "Enter max fizzbuzz number to print:"
+user_input_max = gets.chomp.to_i
+fizz_buzz_test(fizz_buzz_test_cases)
+fizz_buzz_list(user_input_max)
+
+
 
 
