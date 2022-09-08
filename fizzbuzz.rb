2d738ef77306027e96838856eaed798a6719662a
JohHod <john.hodkinson@softwire.com>
2022-09-08 14:33:40 +0100

    FizzBuzz Part 1

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,10 @@
 def fizzbuzz
-  puts 'Hello world!'
+  for number in 1..100
+    print "\n"
+    if number%3==0 then print "Fizz" end
+    if number%5==0 then print "Buzz" end
+    if not number%3==0 and not number%5==0 then print number end
+  end
 end
 
 fizzbuzz
