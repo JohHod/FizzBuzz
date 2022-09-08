d308b1641a3348ed8f53fe6834b7419afdf6a58b
JohHod <john.hodkinson@softwire.com>
2022-09-08 14:48:37 +0100

    FizzBuzzBangBong

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,13 @@
 def fizzbuzz
   for number in 1..100
-    if number%3==0 then print "Fizz" end
-    if number%5==0 then print "Buzz" end
-    if number%7==0 then print "Bang" end
-    if not number%3==0 and not number%5==0 and not number%7==0 then print number end
+    if number%11==0
+      print "Bong"
+    else
+      if number%3==0 then print "Fizz" end
+      if number%5==0 then print "Buzz" end
+      if number%7==0 then print "Bang" end
+      if not number%3==0 and not number%5==0 and not number%7==0 then print number end
+    end
     if number < 100 then print "\n" end
   end
 end
